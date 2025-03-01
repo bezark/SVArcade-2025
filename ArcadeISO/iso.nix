2e2da5a3cf9d86f4002885c963fed7f67fd442a5
bezark <johnbezark@gmail.com>
2025-03-01 14:19:29 -0500

    Update iso.nix

--- ArcadeISO/iso.nix
+++ ArcadeISO/iso.nix
@@ -1,13 +1,21 @@
-{ config, pkgs, ... }: {
+{ config, pkgs, lib, ... }:
+{
   imports = [
     <nixpkgs/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix>
-    <nixpkgs/nixos/modules/installer/cd-dvd/channel.nix>
-    ./config.nix
+    ./configuration.nix
   ];
 
+  # Let your config's "networkmanager.enable" setting win:
+  networking.networkmanager.enable = lib.mkForce true;
+
+  # If your config sets "networking.interfaces" for enp6s0/wlp5s0,
+  # but the minimal ISO also sets them in a conflicting way, forcibly empty them:
+  networking.interfaces = lib.mkForce { };
+
+  # If you had "networking.wireless.enable = true" or something that conflicts, disable it:
+  # networking.wireless.enable = lib.mkForce false;
+
+  # Optionally, ensure it boots on EFI, etc.
   isoImage.makeEfiBootable = true;
   isoImage.makeUsbBootable = true;
-  hardware.enableRedistributableFirmware = true;
-  nixpkgs.config.allowUnfree = true;
-
 }
