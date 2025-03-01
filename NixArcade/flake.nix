b81d5101ed83d193dbd3e8d621c5051f4aa4a8c5
bezark <johnbezark@gmail.com>
2025-03-01 13:25:27 -0500

    Update flake.nix

--- NixArcade/flake.nix
+++ NixArcade/flake.nix
@@ -1,13 +1,16 @@
 {
-
   description = "Nix Arcade ISO Generator";
-  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-22.05;
-  
-  outputs = { self, nixpkgs }: {
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+  # or use: nixos-23.05, or nixos-unstable, etc.
+
+  outputs = { self, nixpkgs }:
+  {
     nixosConfigurations.live = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
-        (nixpkgs + "/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix")
+        # Now this works because nixpkgs.path actually exists:
+        "${nixpkgs.path}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
         ./additional-config.nix
       ];
     };
