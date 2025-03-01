b130495c4b37dfaff4cd93bc66ac092e07d3389f
bezark <johnbezark@gmail.com>
2025-03-01 13:32:04 -0500

    modern?

--- NixArcade/flake.nix
+++ NixArcade/flake.nix
@@ -2,18 +2,22 @@
   description = "Nix Arcade ISO Generator";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
-  # or use: nixos-23.05, or nixos-unstable, etc.
 
   outputs = { self, nixpkgs }:
-  {
-    nixosConfigurations.live = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        # Now this works because nixpkgs.path actually exists:
-        # "${nixpkgs.path}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
-        (nixpkgs + "/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix")
-        ./additional-config.nix
-      ];
+    {
+      nixosConfigurations.live = let
+        # "legacyPackages.x86_64-linux" is the normal package set for x86_64
+        pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      in pkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+
+        modules = [
+          # Pull in the minimal ISO module directly from nixpkgs' flake:
+          pkgs.nixosModules.installer.cd-dvd.installation-cd-minimal
+
+          # Your own config file
+          ./additional-config.nix
+        ];
+      };
     };
-  };
 }
