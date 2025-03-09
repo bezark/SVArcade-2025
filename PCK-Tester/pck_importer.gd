89e67a062ffabb35e63cd77af631d1d56bd5cb8d
bezark <jdb459@nyu.edu>
2025-03-09 13:09:54 -0400

    MVP EchosTale

--- PCK-Tester/pck_importer.gd
+++ PCK-Tester/pck_importer.gd
@@ -8,10 +8,11 @@ func _ready() -> void:
 
 func load_pck():
 	# This could fail if, for example, mod.pck cannot be found.
-	var success = ProjectSettings.load_resource_pack("res://Prison.pck")
+	var success = ProjectSettings.load_resource_pack("res://PCKs/EchosTale.pck")
+	# var success = ProjectSettings.load_resource_pack("res://Prison.pck")
 
 	if success:
 		# Now one can use the assets as if they had them in the project from the start.
-		var imported_scene = load("res://Scene/mainLevel.tscn/")
-		var new_scene = imported_scene.instantiate()
-		add_child(new_scene)
+		# var imported_scene = load("res://Scene/mainLevel.tscn/")
+		var imported_scene = load("res://scenes/levels/Opening_Menu.tscn")
+		get_tree().change_scene_to_packed(imported_scene)
